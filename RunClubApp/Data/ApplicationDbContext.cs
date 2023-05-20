05a456052b2f6bed04c888a5fe679712c9ef93a3
AlbertoJTD <albertojtd@outlook.com>
2023-05-19 21:13:23 -0600

    Add database connection

--- RunClubApp/Data/ApplicationDbContext.cs
+++ RunClubApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace RunClubApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using RunClubApp.Models;
+
+namespace RunClubApp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
     }
 }
--- RunClubApp/Models/AppUser.cs
+++ RunClubApp/Models/AppUser.cs
@@ -1,7 +1,11 @@
-﻿namespace RunClubApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunClubApp.Models
 {
     public class AppUser
     {
+        [Key]
+        public string Id { get; set; }
         public int? Pace { get; set; } // ? means that the property can be null
         public int? Mileage { get; set; }
         public Address? Address { get; set; }
