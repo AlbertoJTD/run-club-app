f76d8a87982595c9071f37deab7a963c6d8e91c3
AlbertoJTD <albertojtd@outlook.com>
2023-05-19 18:19:34 -0600

    Add models

---
+++ RunClubApp/Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunClubApp.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ RunClubApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace RunClubApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; } // ? means that the property can be null
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
