216c08ee2bc39750d08ac5d7bec4b220e7ea1ef8
AlbertoJTD <albertojtd@outlook.com>
2023-05-21 21:40:13 -0600

    Add views

---
+++ RunClubApp/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunClubApp.Data;
+
+namespace RunClubApp.Controllers
+{
+	public class ClubController : Controller
+	{
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+		{
+            var clubs = _context.Clubs.ToList();
+			return View(clubs);
+		}
+	}
+}
