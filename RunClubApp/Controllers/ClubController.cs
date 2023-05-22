4f4ab09e6a26a1bf09538e4a4817c659ea8e93e7
AlbertoJTD <albertojtd@outlook.com>
2023-05-21 21:43:48 -0600

    Remove code

--- RunClubApp/Controllers/ClubController.cs
+++ RunClubApp/Controllers/ClubController.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using RunClubApp.Data;
-=======
->>>>>>> 9e3949a14f13734c6f97bfbb87de466fa2eec73a
 
 namespace RunClubApp.Controllers
 {
 	public class ClubController : Controller
 	{
-<<<<<<< HEAD
         private readonly ApplicationDbContext _context;
 
         public ClubController(ApplicationDbContext context)
@@ -20,11 +16,6 @@ namespace RunClubApp.Controllers
 		{
             var clubs = _context.Clubs.ToList();
 			return View(clubs);
-=======
-		public IActionResult Index()
-		{
-			return View();
->>>>>>> 9e3949a14f13734c6f97bfbb87de466fa2eec73a
 		}
 	}
 }
