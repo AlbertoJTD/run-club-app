3e92c76ce9eec81a0e8ffd71ae3e718b10932b88
AlbertoJTD <albertojtd@outlook.com>
2023-05-22 14:19:41 -0600

    Add Club/Race respository and interfaces

---
+++ RunClubApp/Repository/ClubRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunClubApp.Data;
+using RunClubApp.Interfaces;
+using RunClubApp.Models;
+using System.Diagnostics;
+
+namespace RunClubApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Club club)
+        {
+            _context.Add(club);
+            return Save();
+        }
+
+        public bool Delete(Club club)
+        {
+            _context.Remove(club);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Club>> GetAll()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
+        public async Task<Club> GetByIdAsync(int id)
+        {
+            return await _context.Clubs.FirstOrDefaultAsync(q => q.Id == id);
+        }
+
+        public async Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            return await _context.Clubs.Where(q => q.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Club club)
+        {
+            _context.Update(club);
+            return Save();
+        }
+    }
+}
---
+++ RunClubApp/Repository/RaceRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunClubApp.Data;
+using RunClubApp.Interfaces;
+using RunClubApp.Models;
+
+namespace RunClubApp.Repository
+{
+    public class RaceRepository : IRaceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Race race)
+        {
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race race)
+        {
+            _context.Remove(race);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Race>> GetAll()
+        {
+            return await _context.Races.ToListAsync();
+        }
+
+        public async Task<Race> GetByIdAsync(int id)
+        {
+            return await _context.Races.FirstOrDefaultAsync(q => q.Id == id);
+        }
+
+        public async Task<IEnumerable<Race>> GetAllRacesByCity(string city)
+        {
+            return await _context.Races.Where(q => q.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Race race)
+        {
+            _context.Update(race);
+            return Save();
+        }
+    }
+}
